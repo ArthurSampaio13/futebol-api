d2ba3a17118b96e45c8f78c0c0373a8281a9d809
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-10-30 17:10:28 -0300

    feat: adicionando url do banco

--- view/view.py
+++ view/view.py
@@ -60,20 +60,10 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+DB_URL = os.getenv("DB_URL")
 
 def get_connection():
-    return psycopg2.connect(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        port=DB_PORT
-    )
+    return psycopg2.connect(DB_URL)
 
 def query_db(query, params=None):
     conn = get_connection()
